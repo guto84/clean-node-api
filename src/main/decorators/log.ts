714bb784a0c5fb4f64a75f800fd78e65fc8d1495
guto <gutob84@gmail.com>
2021-06-11 15:11:01 -0300

    refactor: make server error return the error stack

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -6,7 +6,7 @@ export class LogControllerDecorator implements Controller {
   constructor(controller: Controller) {
     this.controller = controller
   }
-  
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
     return httpResponse
