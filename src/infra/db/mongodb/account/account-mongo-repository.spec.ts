33eff87d58816b8fabe29ef25d94ec31e0c70ded
guto <gutob84@gmail.com>
2021-08-27 18:28:28 -0300

    test: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -85,6 +85,23 @@ describe('Account Mongo Repository', () => {
         name: 'any_name',
         email: 'anyemail@email.com',
         password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('anyemail@email.com')
+      expect(account.password).toBe('any_password')
+    })
+
+    test('Should return an account on loadByToken with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'anyemail@email.com',
+        password: 'any_password',
         accessToken: 'any_token'
       })
       const account = await sut.loadByToken('any_token')
